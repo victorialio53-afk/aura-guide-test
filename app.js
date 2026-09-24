a12578642ead6c769cd443f370a026134abbc341
victorialio53-afk <victoria.lio53@gmail.com>
2026-09-24 23:31:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-console.log("AURA.SYSTEM / GUIDE INITIALIZED");
+const homeScreen =
+    document.getElementById("home-screen");
+
+const startScreen =
+    document.getElementById("start-screen");
+
+const openStartButton =
+    document.getElementById("open-start");
+
+const backHomeButton =
+    document.getElementById("back-home");
+
+
+
+function showScreen(screen) {
+
+    const screens =
+        document.querySelectorAll(".screen");
+
+
+    screens.forEach((item) => {
+
+        item.classList.remove("active");
+
+    });
+
+
+    screen.classList.add("active");
+
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+
+
+openStartButton.addEventListener(
+    "click",
+    () => {
+
+        showScreen(startScreen);
+
+    }
+);
+
+
+
+backHomeButton.addEventListener(
+    "click",
+    () => {
+
+        showScreen(homeScreen);
+
+    }
+);
+
+
+
+console.log(
+    "AURA.SYSTEM / GUIDE INITIALIZED"
+);
